2ba5a84b4a529827b9afc6ab1f8db03cc9e013f8
刘石明 <liushiming@quancheng-ec.com>
2017-02-21 14:03:48 +0800

    up

--- plugins/saluki/src/main/java/com/quancheng/pinpoint/plugin/saluki/SalukiConstants.java
+++ plugins/saluki/src/main/java/com/quancheng/pinpoint/plugin/saluki/SalukiConstants.java
@@ -20,8 +20,10 @@ import com.navercorp.pinpoint.common.trace.ServiceTypeFactory;
  */
 public interface SalukiConstants {
 
-    ServiceType   SALUKI_PROVIDER_SERVICE_TYPE = ServiceTypeFactory.of(1111, "SALUKI_PROVIDER", RECORD_STATISTICS);
-    ServiceType   SALUKI_CONSUMER_SERVICE_TYPE = ServiceTypeFactory.of(9132, "SALUKI_CONSUMER", RECORD_STATISTICS);
+    ServiceType   SALUKI_PROVIDER_SERVICE_TYPE = ServiceTypeFactory.of(1111, "SALUKI_PROVIDER", "SALUKI",
+                                                                       RECORD_STATISTICS);
+    ServiceType   SALUKI_CONSUMER_SERVICE_TYPE = ServiceTypeFactory.of(9132, "SALUKI_CONSUMER", "SALUKI",
+                                                                       RECORD_STATISTICS);
     AnnotationKey SALUKI_ARGS_ANNOTATION_KEY   = AnnotationKeyFactory.of(92, "saluki.args");
     AnnotationKey SALUKI_RESULT_ANNOTATION_KEY = AnnotationKeyFactory.of(93, "saluki.result");
 
